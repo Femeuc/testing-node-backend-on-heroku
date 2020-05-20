4793fcab8cf6a2b89f2f8d27665d85673c5c5bce
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-05-20 18:39:25 -0300

    undid last commit

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require('express');
 
-const { Pool, Client } = require('pg')
-
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'teste1',
-    password: '',
-    port: 5432,
-})
-
 const app = express();
 app.use(express.json());
-app.get('/', function(req, res) {
-client.connect()
-client.query('SELECT * FROM test', (err, resq) => {
-  res.json({
-      response: resq.rows[0].name
-  });
-  console.log(err, resq)
-  client.end()
-})
-});
-
-app.post('/test', (request, response) => {
-    client.connect()
-    client.query("INSERT INTO test (name) VALUES ('" + request.body.name + "')");
-    client.end();
+app.get('/', (request, response) => {
+    return response.json({
+      message: "Hellow world!"
+    });
 });
 
 let port = process.env.PORT;
